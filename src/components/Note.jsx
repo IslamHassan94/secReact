b5a2a60366943c17f496be5da4bc63a99ceb46a0
ISLAM <islamhasan@hotmail.com>
2020-07-20 02:40:52 +0200

    first commit

---
+++ src/components/Note.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+
+function Note(){
+
+    return(
+    <div className="note">
+        <h1>
+            Islam Hassan
+        </h1>
+        <p>
+            He is the most completed Fighter in the World , King of Kings Conqueror of The Worlds..
+        </p>
+    </div>
+    )
+};
+
+export default Note;
